ec051abe6014a10a88bdbf7ba469bb612e0a92e8
kelvinkuria <kkuria383@yahoo.com>
2025-05-15 17:13:05 +0400

    app

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-const _ = require('lodash');
-const items = [1, [2, [3, [4, [5]]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+// const { readFile} = require('fs');
 
+// console.log('started a new operation');
+
+// readFile('./content/first.txt','utf8',(err,result)=>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(result);
+//     console.log('completed first task');
+// })
+// console.log('starting next task');
+
+// console.log('first ')
+// setTimeout(()=>{
+//     console.log('second')
+// },0)
+// console.log('third')
+
+// setInterval(()=>{
+//     console.log('hello world')
+// },2000)
+// console.log('I will run first')
+
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    console.log('requestt event')
+    res.end('hello world')
+})
+
+server.listen(5000,()=>{
+    console.log('server is listening on port 5000')
+})
