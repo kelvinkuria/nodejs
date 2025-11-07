2f8d53b6aa834a34ce37c9f316d9fb0c4c5c7db1
kelvinkuria <kkuria383@yahoo.com>
2025-11-07 19:37:42 +0400

    packages

--- hello.js
+++ hello.js
@@ -1,23 +1,5 @@
-const {readFile,writeFile} = require('fs')
+const _ = require('lodash')
 
-readFile('./content/first.txt','utf-8',(err,result)=>{
-if(err){
-    console.log(err)
-    return
-}
-const first = result;
-readFile('./content/second.txt','utf-8',(err,result)=>{
-    if (err){
-        console.log(err)
-        return
-    }
-    const second = result
-writeFile('./content/result-async.txt',`Here is the result:${first}, ${second}`,{flag: 'a'},(err,result)=>{
-    if(err){
-        console.log(err)
-        return
-    }
-    console.log(result)
-})
-})
-})
+const items = [1,[2,[3,[4]]]]
+const newItems = _.flattenDeep(items);
+console.log(newItems)
